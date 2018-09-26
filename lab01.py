12cf63da9d320e5d4990b4b29b939f7ea1bc7fe6
Roman Khomenko <romankhomenko1995@gmail.com>
2018-09-26 09:15:25 +0300

    P1D implicit FDM added

--- lab01.py
+++ lab01.py
@@ -18,10 +18,16 @@ class P1D:
         self.mu2 = mu2
 
 
-def p1d_explicit_fdm(p1d, x1, x2, steps_count, t1, t2, sigma):
+def calculate_steps(p1d, x1, x2, steps_count, t1, t2, sigma):
     n = steps_count + 1
     h = (x2 - x1) / steps_count
     tau = sigma * h ** 2 / p1d.a
+
+    return n, h, tau
+
+
+def p1d_explicit_fdm(p1d, x1, x2, steps_count, t1, t2, sigma):
+    n, h, tau = calculate_steps(p1d, x1, x2, steps_count, t1, t2, sigma)
 
     u1 = np.zeros(n)
     u2 = np.zeros(n)
@@ -53,7 +59,7 @@ def p1d_explicit_fdm(p1d, x1, x2, steps_count, t1, t2, sigma):
         omega = tau * p1d.b / 2 / h
         for i in range(1, n - 1):
             u[i] = (sigma + omega) * u_prev[i + 1] \
-                + (1 - 2 * sigma + p1d.c) * u_prev[i] \
+                + (1 - 2 * sigma + tau * p1d.c) * u_prev[i] \
                 + (sigma - omega) * u_prev[i - 1] \
                 + tau * p1d.f(xk[i], t)
 
@@ -77,8 +83,87 @@ def p1d_explicit_fdm(p1d, x1, x2, steps_count, t1, t2, sigma):
         plt.plot(xk, u)
 
 
-p1d = P1D(a=1, b=0, c=0, f=lambda x, t: 0.5 * np.exp(-0.5 * t) * np.sin(x),
+def tdma(u, a, b, c, d):
+    n = len(u)
+    k1 = np.zeros(n)
+    k2 = np.zeros(n)
+
+    k1[0] = -c[0] / b[0]
+    k2[0] = d[0] / b[0]
+
+    for i in range(1, n - 1):
+        k1[i] = - c[i] / (b[i] + a[i] * k1[i - 1])
+        k2[i] = (d[i] - a[i] * k2[i - 1]) / (b[i] + a[i] * k1[i - 1])
+
+    k1[n - 1] = 0
+    k2[n - 1] = (d[n - 1] - a[n - 1] * k2[n - 2]) / (b[n - 1] + a[n - 1] * k1[n - 2])
+
+    u[n - 1] = k2[n - 1]
+
+    for i in reversed(range(0, n - 1)):
+        u[i] = k1[i] * u[i + 1] + k2[i]
+
+
+def p1d_implicit_fdm(p1d, x1, x2, steps_count, t1, t2, sigma):
+    n, h, tau = calculate_steps(p1d, x1, x2, steps_count, t1, t2, sigma)
+    omega = tau * p1d.b / 2 / h
+
+    u1 = np.zeros(n)
+    u2 = np.zeros(n)
+    a = np.zeros(n)
+    b = np.zeros(n)
+    c = np.zeros(n)
+    d = np.zeros(n)
+    xk = np.zeros(n)
+
+    a[0] = 0
+    b[0] = 1
+    c[0] = p1d.alpha / h / (p1d.beta - p1d.alpha / h)
+
+    for i in range(1, n - 1):
+        a[i] = omega - sigma
+        b[i] = 1 + 2 * sigma - p1d.c * tau
+        c[i] = -(sigma + omega)
+
+    a[n - 1] = - p1d.gamma / h / (p1d.delta - p1d.gamma / h)
+    b[n - 1] = 1
+    c[n - 1] = 0
+
+    for i in range(0, n):
+        xk[i] = x1 + i * h
+        u1[i] = p1d.phi(xk[i])
+
+    tk = t1 + tau
+    step = 0
+    while tk <= t2:
+        if step % 2 == 0:
+            u_prev = u1
+            u = u2
+        else:
+            u_prev = u2
+            u = u1
+
+        d[0] = 1 / (p1d.beta - p1d.alpha / h) * p1d.mu1(tk)
+        for i in range(1, n - 1):
+            d[i] = u_prev[i] + tau * p1d.f(xk[i], tk)
+        d[n - 1] = - 1 / (p1d.delta + p1d.gamma / h) * p1d.mu2(tk)
+
+        tdma(u, a, b, c, d)
+        plt.plot(xk, u)
+
+        tk += tau
+        step += 1
+
+
+p1d_7 = P1D(a=1, b=0, c=0, f=lambda x, t: 0.5 * np.exp(-0.5 * t) * np.sin(x),
           phi=np.sin, alpha=1, beta=0, mu1=lambda t: np.exp(-0.5 * t),
           gamma=1, delta=0, mu2=lambda t: -np.exp(-0.5 * t))
-p1d_explicit_fdm(p1d, 0, np.pi, 10, 0, 2, 0.45)
+a = 1
+b = 1
+c = -1
+p1d_10 = P1D(a=a, b=b, c=c, f=lambda x, t: 0,
+          phi=np.sin, alpha=1, beta=1, mu1=lambda t: np.exp((c - a) * t) * (np.cos(b * t) + np.sin(b * t)),
+          gamma=1, delta=1, mu2=lambda t: -np.exp((c - a) * t) * (np.cos(b * t) + np.sin(b * t)))
+# p1d_explicit_fdm(p1d_10, 0, np.pi, 10, 0, 2, 0.45)
+p1d_implicit_fdm(p1d_7, 0, np.pi, 10, 0, 2, 0.4)
 plt.show()
