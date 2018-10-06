8f9d5b53ea5522891018b5e5ef0026539eeef966
Roman Khomenko <romankhomenko1995@gmail.com>
2018-10-07 01:18:54 +0300

    Optimization and bug fix

--- lab01.py
+++ lab01.py
@@ -4,7 +4,7 @@ import tkinter
 
 
 class P1D:
-    def __init__(self, a, b, c, f, phi, alpha, beta, mu1, gamma, delta, mu2):
+    def __init__(self, a, b, c, f, phi, alpha, beta, mu1, gamma, delta, mu2, solution):
         self.a = a
         self.b = b
         self.c = c
@@ -16,6 +16,7 @@ class P1D:
         self.gamma = gamma
         self.delta = delta
         self.mu2 = mu2
+        self.solution = solution
 
 
 def calculate_steps(p1d, x1, x2, steps_count, t1, t2, sigma):
@@ -52,9 +53,6 @@ def p1d_explicit_fdm(p1d, x1, x2, steps_count, t1, t2, sigma):
     u1[0] = u_left(u1, 0)
     u1[n - 1] = u_right(u1, 0)
 
-    # Debug plot
-    plt.plot(xk, u1)
-
     def calculate_u(u, u_prev, t):
         omega = tau * p1d.b / 2 / h
         for i in range(1, n - 1):
@@ -80,13 +78,11 @@ def p1d_explicit_fdm(p1d, x1, x2, steps_count, t1, t2, sigma):
         tk += tau
         step += 1
 
-        plt.plot(xk, u)
+    return xk, u
 
 
-def tdma(u, a, b, c, d):
+def tdma(u, a, b, c, d, k1, k2):
     n = len(u)
-    k1 = np.zeros(n)
-    k2 = np.zeros(n)
 
     k1[0] = -c[0] / b[0]
     k2[0] = d[0] / b[0]
@@ -115,6 +111,8 @@ def p1d_implicit_fdm(p1d, x1, x2, steps_count, t1, t2, sigma):
     c = np.zeros(n)
     d = np.zeros(n)
     xk = np.zeros(n)
+    k1 = np.zeros(n)
+    k2 = np.zeros(n)
 
     a[0] = 0
     b[0] = 1
@@ -148,11 +146,12 @@ def p1d_implicit_fdm(p1d, x1, x2, steps_count, t1, t2, sigma):
             d[i] = u_prev[i] + tau * p1d.f(xk[i], tk)
         d[n - 1] = 1 / (p1d.delta + p1d.gamma / h) * p1d.mu2(tk)
 
-        tdma(u, a, b, c, d)
-        plt.plot(xk, u)
+        tdma(u, a, b, c, d, k1, k2)
 
         tk += tau
         step += 1
+
+    return xk, u
 
 
 def p1d_crank_nicolson_fdm(p1d, x1, x2, steps_count, t1, t2, sigma, teta):
@@ -166,15 +165,17 @@ def p1d_crank_nicolson_fdm(p1d, x1, x2, steps_count, t1, t2, sigma, teta):
     c = np.zeros(n)
     d = np.zeros(n)
     xk = np.zeros(n)
+    k1 = np.zeros(n)
+    k2 = np.zeros(n)
 
     a[0] = 0
     b[0] = 1
     c[0] = p1d.alpha / h / (p1d.beta - p1d.alpha / h)
 
     for i in range(1, n - 1):
-        a[i] = omega - teta * sigma
-        b[i] = 1 + 2 * teta * sigma - p1d.c * tau
-        c[i] = - (teta * sigma + omega)
+        a[i] = teta * (omega - sigma)
+        b[i] = 1 + teta * (2 * sigma - p1d.c * tau)
+        c[i] = - teta * (sigma + omega)
 
     a[n - 1] = - p1d.gamma / h / (p1d.delta + p1d.gamma / h)
     b[n - 1] = 1
@@ -185,6 +186,7 @@ def p1d_crank_nicolson_fdm(p1d, x1, x2, steps_count, t1, t2, sigma, teta):
         u1[i] = p1d.phi(xk[i])
 
     f = np.zeros(n)
+    tk_prev = t1
     tk = t1 + tau
     step = 0
     while tk <= t2:
@@ -197,33 +199,50 @@ def p1d_crank_nicolson_fdm(p1d, x1, x2, steps_count, t1, t2, sigma, teta):
 
         d[0] = 1 / (p1d.beta - p1d.alpha / h) * p1d.mu1(tk)
         for i in range(1, n - 1):
-            d[i] = (sigma * (1 - teta) + omega) * u_prev[i + 1] + \
-                   (1 - 2 * sigma * (1 - teta) + p1d.c * tau) * u_prev[i] + \
-                   (sigma * (1 - teta) - omega) * u_prev[i - 1] + \
-                   tau * p1d.f(xk[i], tk)
+            d[i] = (1 - teta) * (sigma + omega) * u_prev[i + 1] + \
+                   (1 + (1 - teta) * (- 2 * sigma + p1d.c * tau)) * u_prev[i] + \
+                   (1 - teta) * (sigma - omega) * u_prev[i - 1] + \
+                   teta * tau * p1d.f(xk[i], tk_prev + teta * tau) + \
+                   (1 - teta) * tau * p1d.f(xk[i], tk_prev + (1 - teta) * tau)
+
         d[n - 1] = 1 / (p1d.delta + p1d.gamma / h) * p1d.mu2(tk)
 
-        for i in range(0, n):
-            f[i] = np.abs(u[i] - p1d.f(xk[i], tk))
-        print(f)
+        tdma(u, a, b, c, d, k1, k2)
 
-        tdma(u, a, b, c, d)
-        plt.plot(xk, u)
-
+        tk_prev = tk
         tk += tau
         step += 1
 
+    return xk, u
 
-p1d_7 = P1D(a=1, b=0, c=0, f=lambda x, t: 0.5 * np.exp(-0.5 * t) * np.sin(x),
-            phi=np.sin, alpha=1, beta=0, mu1=lambda t: np.exp(-0.5 * t),
-            gamma=1, delta=0, mu2=lambda t: -np.exp(-0.5 * t))
+
+def error_and_plot(p1d, xk, u, t):
+    solution = [p1d.solution(x, t) for x in xk]
+    error = [np.abs(solution[i] - u[i]) for i in range(len(xk))]
+    print(error)
+    plt.plot(xk, u, label='Calculated')
+    plt.plot(xk, solution, label='Solution', ls='--')
+    plt.legend()
+    plt.show()
+
+
 a = 1
 b = 1
 c = -1
+t1 = 0
+t2 = 1
+sigma = 0.45
+p1d_7 = P1D(a=a, b=0, c=0, f=lambda x, t: 0.5 * np.exp(-0.5 * t) * np.sin(x),
+            phi=np.sin, alpha=1, beta=0, mu1=lambda t: np.exp(-0.5 * t),
+            gamma=1, delta=0, mu2=lambda t: -np.exp(-0.5 * t),
+            solution=lambda x, t: np.exp(-0.5 * t) * np.sin(x))
 p1d_10 = P1D(a=a, b=b, c=c, f=lambda x, t: 0,
              phi=np.sin, alpha=1, beta=1, mu1=lambda t: np.exp((c - a) * t) * (np.cos(b * t) + np.sin(b * t)),
-             gamma=1, delta=1, mu2=lambda t: -np.exp((c - a) * t) * (np.cos(b * t) + np.sin(b * t)))
-# p1d_explicit_fdm(p1d_10, 0, np.pi, 10, 0, 2, 0.45)
-# p1d_implicit_fdm(p1d_7, 0, np.pi, 10, 0, 2, 0.4)
-p1d_crank_nicolson_fdm(p1d_7, 0, np.pi, 10, 0, 2, 0.4, 0.5)
-plt.show()
+             gamma=1, delta=1, mu2=lambda t: -np.exp((c - a) * t) * (np.cos(b * t) + np.sin(b * t)),
+             solution=lambda x, t: np.exp((c - a) * t) * np.sin(x + b * t))
+# xk, u = p1d_explicit_fdm(p1d_10, 0, np.pi, 100, t1, t2, sigma)
+# error_and_plot(p1d_10, xk, u, t2)
+# xk, u = p1d_implicit_fdm(p1d_10, 0, np.pi, 20, t1, t2, 0.4)
+# error_and_plot(p1d_10, xk, u, t2)
+xk, u = p1d_crank_nicolson_fdm(p1d_7, 0, np.pi, 20, t1, t2, sigma, 0.5)
+error_and_plot(p1d_7, xk, u, t2)
