e08722ae3211bdd8bd5545150ec321b7e36687fb
Roman Khomenko <romankhomenko1995@gmail.com>
2019-01-15 00:27:42 +0300

    Leibmann

--- lab03/E2D.py
+++ lab03/E2D.py
@@ -3,20 +3,69 @@ import numba as nb
 
 
 class EquationParams:
-    def __init__(self, a, b, c, d, e, f,
+    def __init__(self, x, y, a, b, c, d,
                  mu1, mu2, mu3, mu4,
                  solution):
+        self.x = x
+        self.y = y
         self.a = a
         self.b = b
         self.c = c
         self.d = d
-        self.e = e
-        self.f = f
         self.mu1 = mu1
         self.mu2 = mu2
         self.mu3 = mu3
         self.mu4 = mu4
         self.solution = solution
+
+
+@nb.jit(nopython=True)
+def norm_inf(arr, nx, ny):
+    result = -1
+    for j in range(0, ny):
+        sum = 0
+        for i in range(0, nx):
+            sum += arr[i][j]
+        if result < sum:
+            result = sum
+
+    return result
+
+
+@nb.jit(nopython=True)
+def leibman_calc(u1, u2, nx, ny, a, b, c, d, e, eps):
+    counter = 0
+    while True:
+        for i in range(1, nx - 1):
+            for j in range(1, ny - 1):
+                u2[i][j] = b * u1[i + 1][j] + c * u1[i - 1][j] + \
+                           d * u1[i][j + 1] + e * u1[i][j - 1]
+                u2[i][j] /= a
+
+        if norm_inf(np.abs(u2 - u1), nx, ny) < eps:
+            break
+
+        counter += 1
+        u1, u2 = u2, u1
+
+    return u1
+
+
+@nb.jit(nopython=True)
+def simple_calc(u1, u2, n, eps):
+    counter = 0
+    while True:
+        for i in range(1, n - 1):
+            for j in range(1, n - 1):
+                u2[i][j] = 1 / 4 * (u1[i + 1][j] + u1[i - 1][j] +
+                                    u1[i][j + 1] + u1[i][j - 1])
+        if norm_inf(np.abs(u2 - u1), n, n) < eps:
+            break
+
+        counter += 1
+        u1, u2 = u2, u1
+
+    return u1
 
 
 def calculate_grid(start, end, steps):
@@ -25,7 +74,32 @@ def calculate_grid(start, end, steps):
     return n, h
 
 
-def e2d_solver_leibmann(e2d, x1, x2, y1, y2, steps_x, steps_y, eps):
+def e2d_solver_simple(e2d, steps_x, steps_y, eps):
+    x1, x2 = e2d.x
+    y1, y2 = e2d.y
+    n, h = calculate_grid(x1, x2, steps_x)
+
+    u1 = np.zeros((n, n), dtype=np.float64)
+    u2 = np.zeros((n, n), dtype=np.float64)
+
+    for j in range(0, n):
+        y = y1 + j * h
+        u1[0][j] = u2[0][j] = e2d.mu1(y)
+        u1[n - 1][j] = u2[n - 1][j] = e2d.mu2(y)
+
+    for i in range(0, n):
+        x = x1 + i * h
+        u1[i][0] = u2[i][0] = e2d.mu3(x)
+        u1[i][n - 1] = u2[i][n - 1] = e2d.mu4(x)
+
+    u = simple_calc(u1, u2, n, eps)
+
+    return u, n, n, h, h
+
+
+def e2d_solver_leibmann(e2d, steps_x, steps_y, eps):
+    x1, x2 = e2d.x
+    y1, y2 = e2d.y
     nx, hx = calculate_grid(x1, x2, steps_x)
     ny, hy = calculate_grid(y1, y2, steps_y)
 
@@ -42,24 +116,12 @@ def e2d_solver_leibmann(e2d, x1, x2, y1, y2, steps_x, steps_y, eps):
         u1[i][0] = u2[i][0] = e2d.mu3(x)
         u1[i][ny - 1] = u2[i][ny - 1] = e2d.mu4(x)
 
-    A = - (2 / hx ** 2 + 2 * e2d.a / hy ** 2 - e2d.d)
-    B = e2d.b / 2 / hx - 1 / hx ** 2
-    C = - (e2d.b / 2 / hx + 1 / hx ** 2)
-    D = e2d.c / 2 / hy - e2d.a / hy ** 2
-    E = - (e2d.a / hy ** 2 + e2d.c / 2 / hy)
+    a = - (2 / hx ** 2 + 2 * e2d.a / hy ** 2 + e2d.d)
+    b = e2d.b / 2 / hx - 1 / hx ** 2
+    c = - (e2d.b / 2 / hx + 1 / hx ** 2)
+    d = e2d.c / 2 / hy - e2d.a / hy ** 2
+    e = - (e2d.a / hy ** 2 + e2d.c / 2 / hy)
 
-    counter = 0
-    while counter != (nx - 2) * (ny - 2):
-        counter = 0
-        for i in range(1, nx - 1):
-            for j in range(1, ny - 1):
-                u2[i][j] = B / A * u1[i + 1][j] + C / A * u1[i - 1][j] + \
-                    D / A * u1[i][j + 1] + E / A * u1[i][j - 1] + \
-                    e2d.e / A * e2d.f(x1 + i * hx, y1 + j * hy)
+    u = leibman_calc(u1, u2, nx, ny, a, b, c, d, e, eps)
 
-                if abs(u2[i][j] - u1[i][j]) < eps:
-                    counter += 1
-        print(counter)
-        u1, u2 = u2, u1
-
-    return u1, nx, ny, hx, hy
+    return u, nx, ny, hx, hy
--- lab03/main.py
+++ lab03/main.py
@@ -12,26 +12,62 @@ def plot_data(x1, x2, y1, y2, hx, hy, sol):
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.plot_surface(X, Y, Z)
+    ax.plot_wireframe(X, Y, Z)
     plt.show()
 
-e2d = EquationParams(a=1, b=-2, c=-2, d=-4, e=0,
-                     f=lambda x, y: 0,
-                     mu1=lambda y: np.exp(-y) * np.cos(y),
-                     mu2=lambda y: 0,
-                     mu3=lambda x: np.exp(-x) * np.cos(x),
-                     mu4=lambda x: 0,
-                     solution=lambda x, y: np.exp(-x - y) * np.cos(x) * np.cos(y))
+e2d10 = EquationParams(x=(0, np.pi / 2),
+                       y=(0, np.pi / 2),
+                       a=1, b=-2, c=-2, d=-4,
+                       mu1=lambda y: np.exp(-y) * np.cos(y),
+                       mu2=lambda y: 0,
+                       mu3=lambda x: np.exp(-x) * np.cos(x),
+                       mu4=lambda x: 0,
+                       solution=lambda x, y: np.exp(-x - y) * np.cos(x) * np.cos(y))
 
-res, nx, ny, hx, hy = e2d_solver_leibmann(e2d, 0, np.pi / 2, 0, np.pi / 2, 200, 200, 1e-5)
-print(res)
-print("***")
+e2d7 = EquationParams(x=(0, np.pi / 2),
+                      y=(0, np.pi / 2),
+                      a=1, b=0, c=0, d=-2,
+                      mu1=lambda y: np.cos(y),
+                      mu2=lambda y: 0,
+                      mu3=lambda x: np.cos(x),
+                      mu4=lambda x: 0,
+                      solution=lambda x, y: np.cos(x) * np.cos(y))
 
-sol = np.zeros((nx, ny), dtype=np.float64)
-for i in range(0, nx):
-    for j in range(0, ny):
-        sol[i][j] = e2d.solution(i * hx, j * hy)
-print(sol)
-print(np.linalg.norm(np.abs(sol - res), ord=np.inf))
-plot_data(0, np.pi / 2, 0, np.pi / 2, hx, hy, sol)
-plot_data(0, np.pi / 2, 0, np.pi / 2, hx, hy, res)
+
+e2d1 = EquationParams(x=(0, 1),
+                      y=(0, 1),
+                      a=1, b=0, c=0, d=0,
+                      mu1=lambda y: y,
+                      mu2=lambda y: 1 + y,
+                      mu3=lambda x: x,
+                      mu4=lambda x: 1 + x,
+                      solution=lambda x, y: x + y)
+
+e2d0 = EquationParams(x=(0, 1),
+                      y=(0, 1),
+                      a=1, b=0, c=0, d=0,
+                      mu1=lambda y: 1000,
+                      mu2=lambda y: 250,
+                      mu3=lambda x: 500,
+                      mu4=lambda x: 750,
+                      solution=lambda x, y: 0)
+
+
+def calc(e2d, n, eps):
+    x1, x2 = e2d.x
+    y1, y2 = e2d.y
+    res1, nx, ny, hx, hy = e2d_solver_leibmann(e2d, n, n, eps)
+
+    sol = np.zeros((nx, ny), dtype=np.float64)
+    for i in range(0, nx):
+        for j in range(0, ny):
+            sol[i][j] = e2d.solution(x1 + i * hx, y1 + j * hy)
+
+    print(n, norm_inf(np.abs(sol - res1), nx, ny))
+    # plot_data(x1, x2, y1, y2, hx, hy, sol)
+    # plot_data(x1, x2, y1, y2, hx, hy, res1)
+    # plot_data(x1, x2, y1, y2, hx, hy, res2)
+
+
+for n in range(5, 100):
+    calc(e2d10, n, 1e-6)
